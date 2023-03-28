0efee9b013c8fd36fc263631485c2cfde1341533
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-03-28 11:06:00 +0200

    changed path

--- src/combine_lai_model_results.py
+++ src/combine_lai_model_results.py
@@ -183,7 +183,7 @@ def combine_lai_model_results(
 if __name__ == '__main__':
     
     test_sites_dir = Path('../data/Test_Sites')
-    s2_trait_dir = Path('../S2_Traits')
+    s2_trait_dir = Path('/home/graflu/public/Evaluation/Projects/KP0031_lgraf_PhenomEn/04_LaaL/S2_Traits')
     meteo_dir = Path('../data/Meteo')
 
     combine_lai_model_results(
