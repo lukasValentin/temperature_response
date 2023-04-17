7c61e865fd73198b1ae9496c9fc8a5f1fcbc5c4f
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-04-17 09:01:44 +0200

    extracting viewing and illumination angles from STAC

--- src/laal/extract_s2_spectra.py
+++ src/laal/extract_s2_spectra.py
@@ -6,6 +6,13 @@ angles and store results in a GeoPackage (i.e., SQL database).
 '''
 
 import geopandas as gpd
+import numpy as np
+import os
+import pandas as pd
+import planetary_computer
+import tempfile
+import urllib.request
+import uuid
 
 from datetime import datetime
 from eodal.config import get_settings
@@ -19,14 +26,38 @@ from typing import Any, Dict, List
 Settings = get_settings()
 Settings.USE_STAC = True
 
-months_of_interest: List[int] = [3,4,5,6]
+months_of_interest: List[int] = [3] # [3,4,5,6]
 cloudy_pixel_percentage: int = 50 # percent
+parcel_cloudy_pixel_percentage: int = 20 # percent
 snow_ice_percentage = 0 # percent
 processing_level: str = 'Level-2A'
 collection: str = 'sentinel2-msi'
 
 # Sentinel-2 bands to extract and use for PROSAIL runs
 band_selection: List[str] = ['B02','B03','B04','B05','B06','B07','B8A','B11','B12']
+
+def angles_from_mspc(url: str) -> Dict[str, float]:
+    """
+    Extract viewing and illumination angles from MS Planetary Computer
+    metadata XML (this is a work-around until STAC provides the angles
+    directly)
+
+    :param url:
+        URL to the metadata XML file
+    :returns:
+        extracted angles as dictionary
+    """
+    response = urllib.request.urlopen(planetary_computer.sign_url(url)).read()
+    temp_file = os.path.join(tempfile.gettempdir(),f'{uuid.uuid4()}.xml')
+    with open(temp_file, 'wb') as dst:
+        dst.write(response)
+
+    from eodal.metadata.sentinel2.parsing import parse_MTD_TL
+    metadata = parse_MTD_TL(in_file=temp_file)
+    # get sensor zenith and azimuth angle
+    sensor_angles = ['SENSOR_ZENITH_ANGLE', 'SENSOR_AZIMUTH_ANGLE']
+    sensor_angle_dict = {k:v for k,v in metadata.items() if k in sensor_angles}
+    return sensor_angle_dict
 
 def preprocess_sentinel2_scenes(
     ds: Sentinel2,
@@ -82,6 +113,29 @@ def config_mapper(
     )
     return mapper_configs
 
+def parcel_cloud_cover(raster: np.ma.MaskedArray) -> float:
+    """
+    Check how much percent of the pixels in a parcel
+    are cloudy
+
+    :param raster:
+        raster with SCL values
+    :returns:
+        percentage of valid SCL pixels (classes 4 and 5) in %
+    """
+    # get the non-masked SCL values
+    _raster = raster.copy()
+    _raster = _raster.compressed()
+    scl_classes, counts = np.unique(_raster, return_counts=True)
+    scl_counts = dict(zip(scl_classes, counts))
+    # count the occurence of SCL classes 4 and 5
+    valid_counts = scl_counts.get(4, 0) + scl_counts.get(5, 0)
+    # in case the entire parcel is masked return 100
+    if _raster.size == 0:
+        return 100
+    # else calculate the percentage of valid pixel
+    return (1 - valid_counts / _raster.size) * 100
+
 def extract_s2_spectra(
     ww_polys_dir: Path,
     out_dir: Path
@@ -115,6 +169,61 @@ def extract_s2_spectra(
             mapper.metadata = mapper.metadata[
                 mapper.metadata.tile_id == s2_tile
             ].copy()
+
+            # load scenes
+            mapper.load_scenes(scene_kwargs)
+            # calculate zonal statistics of the SCL layer. If more than XX% of
+            # the parcel pixel is cloudy remove the record
+            scl_stats = mapper.data.get_feature_timeseries(
+                vector_features=polys,
+                band_selection=['SCL'],
+                method=[parcel_cloud_cover]
+            )
+            sel_cols = ['eodal_id', 'parcel_cloud_cover', 'acquisition_time', 'geometry']
+            scl_stats = scl_stats[sel_cols].copy()
+            # calculate the median reflectance of the optical bands
+            refl_stats = mapper.data.get_feature_timeseries(
+                vector_features=polys,
+                band_selection=band_selection,
+                method=['median']
+            )
+            sel_cols = ['eodal_id', 'band_name', 'median', 'acquisition_time']
+            refl_stats = refl_stats[sel_cols].copy()
+            # join SCL and reflectance information
+            joined = pd.merge(
+                left=refl_stats,
+                right=scl_stats,
+                on=['eodal_id', 'acquisition_time']
+            )
+            # drop records where the cloudy pixel percentage is higher than the threshold
+            joined = joined[
+                joined.parcel_cloud_cover <= parcel_cloudy_pixel_percentage
+            ].copy()
+            joined['geometry_wkt'] = joined.geometry.apply(lambda x: x.wkt)
+
+            # reformat dataframe into wide format
+            df = joined.pivot_table(
+                index=['eodal_id', 'acquisition_time', 'parcel_cloud_cover', 'geometry_wkt'],
+                columns='band_name'
+            )
+            # re-set the index
+            df = df.reset_index()
+            new_columns = [x[0] if x[0] != 'median' else x[1] for x in df.columns]
+            df.columns = new_columns
+            # convert pack to GeoDataFrame
+            gdf = gpd.GeoDataFrame(
+                df,
+                geometry=gpd.GeoSeries.from_wkt(df.geometry_wkt),
+                crs=scl_stats.crs
+            )
+            gdf.drop(columns=['geometry_wkt'], inplace=True)
+
+            # get scene viewing  and illumination angles from metadata xml
+            for _, metadata_item in mapper.metadata.iterrows():
+                url = metadata_item.assets['granule-metadata']['href']
+                angle_dict = angles_from_mspc(url=url)
+                # TODO: join the angles and the extracted spectra
+            
 
 if __name__ == '__main__':
 
