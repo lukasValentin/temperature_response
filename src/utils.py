f8782a497f1f00e2bc5ab2991b29728b25ba0076
lukas <lukasvalentin.graf@usys.ethz.ch>
2023-04-24 09:32:51 +0200

    parsing meteo data from MeteoSwiss

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+'''
+Some useful functions for preparing data
+'''
+
+import pandas as pd
+
+from pathlib import Path
+
+def from_meteoswiss(fpath: Path) -> pd.DataFrame:
+    """
+    read data from MeteoSwiss and convert it into CSV
+
+    :param fpath:
+        original data as delivered from MeteoSwiss (IDAWEB)
+    :returns:
+        DataFrame with internally used conventions
+    """
+    df = pd.read_csv(fpath, sep=';')
+    df.rename(columns={'tre200h0': 'T_mean'}, inplace=True)
+    df.drop(columns=['stn'], inplace=True)
+    df['time'] = pd.to_datetime(df.time, format='%Y%m%d%H')
+    return df
+
+if __name__ == '__main__':
+
+    fpath = Path('data/Meteo/order111046/order_111046_data.txt')
+    df_meteo = from_meteoswiss(fpath=fpath)
+    # save data
+    fpath_out = fpath.parent.parent.joinpath('SwissFutureFarm_Meteo_hourly.csv')
+    df_meteo.to_csv(fpath_out)
